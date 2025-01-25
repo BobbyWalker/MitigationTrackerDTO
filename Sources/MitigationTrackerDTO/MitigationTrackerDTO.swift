3371bc7a97b12231ec1047d9d6e532b64df716d4
Bobby Walker <BobbyWalker@users.noreply.github.com>
2025-01-25 10:53:52 -0600

    Move DTOs into shared package

--- Sources/MitigationTrackerDTO/MitigationTrackerDTO.swift
+++ Sources/MitigationTrackerDTO/MitigationTrackerDTO.swift
@@ -1,2 +1,27 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+
+public struct LoginResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String?
+    public let token: String?
+    public let userId: UUID?
+    
+    public init(error: Bool = false, token: String? = nil, userId: UUID? = nil, reason: String? = nil) {
+        self.error = error
+        self.token = token
+        self.userId = userId
+        self.reason = reason
+    }
+}
+
+public struct RegisterResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+    
+    public init(error: Bool = false, reason: String? = nil) {
+        self.error = error
+        self.reason = reason
+    }
+}
